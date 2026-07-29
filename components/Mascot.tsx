be90bb980372def75e2f7f3aca1627942734962e
bhaviksoni228-pixel <bhaviksoni228@gmail.com>
2026-07-29 12:44:55 +0530

    Update Mascot.tsx

--- components/Mascot.tsx
+++ components/Mascot.tsx
@@ -31,11 +31,15 @@ export default function Mascot() {
         style={{ perspective: 1200 }}
       >
         {/* orbit rings */}
-        <div className="absolute top-1/2 left-1/2 w-[115%] max-w-[420px] aspect-square -translate-x-1/2 -translate-y-1/2 rounded-full border border-grey-4 animate-spin-slow" />
+        <div className="absolute top-1/2 left-1/2 w-[115%] max-w-[420px] aspect-square -translate-x-1/2 -translate-y-1/2 rounded-full border border-grey-4 animate-spin-slow">
+          <div className="absolute top-0 left-1/2 w-2.5 h-2.5 -translate-x-1/2 -translate-y-1/2 rounded-full bg-white shadow-[0_0_10px_3px_rgba(255,255,255,0.7)]" />
+        </div>
         <div
           className="absolute top-1/2 left-1/2 w-[85%] max-w-[320px] aspect-square -translate-x-1/2 -translate-y-1/2 rounded-full border border-grey-3 animate-spin-slow"
           style={{ animationDuration: '60s', animationDirection: 'reverse' }}
-        />
+        >
+          <div className="absolute bottom-0 left-1/2 w-2 h-2 -translate-x-1/2 translate-y-1/2 rounded-full bg-white shadow-[0_0_8px_2px_rgba(255,255,255,0.6)]" />
+        </div>
 
         <motion.div
           className="relative z-10 w-[52vw] max-w-[260px]"
