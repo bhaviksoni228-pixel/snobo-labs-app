1db375b4a1fffbced312aebdb2bb3623d3733df3
bhaviksoni228-pixel <bhaviksoni228@gmail.com>
2026-07-31 10:35:30 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,12 @@ module.exports = {
         black: '#000000',
         white: '#ffffff',
         grey: {
-          1: '#0a0a0a',
-          2: '#1a1a1a',
-          3: '#3a3a3a',
-          4: '#7a7a7a',
-          5: '#c9c9c9',
-        },
+  1: '#0a0a0a',
+  2: '#242424',
+  3: '#525252',
+  4: '#9c9c9c',
+  5: '#e2e2e2',
+},
       },
       fontFamily: {
         display: ['var(--font-space-grotesk)', 'sans-serif'],
